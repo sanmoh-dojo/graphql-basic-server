011083766c5d303a45cd69e29b457b17f0cf94b3
Sanmoh Hombal <devsanmoh@gmail.com>
2020-04-27 23:49:00 +0530

    Basic with schema and express-graphql

--- graphql-server/app.js
+++ graphql-server/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
 
 const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    // pass in schema
+  })
+);
 
 app.listen(4000, () => {
   console.log("now listening to port 4000");
