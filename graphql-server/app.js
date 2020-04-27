25af898cc79b5aacb7a0d74e2d60bf39ed4ee51b
Sanmoh Hombal <devsanmoh@gmail.com>
2020-04-28 00:45:13 +0530

    Mongoose Connected

--- graphql-server/app.js
+++ graphql-server/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://Sanmoh:S%40nm0h0mbal@cluster0-f7uwv.mongodb.net/test?retryWrites=true&w=majority",
+  {useNewUrlParser: true}
+);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 app.use("/graphql", graphqlHTTP({schema, graphql: true}));
 
