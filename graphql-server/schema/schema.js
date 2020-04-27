8e70be9210a8fcdb04ee0deb29543f839c6d8e1b
Sanmoh Hombal <devsanmoh@gmail.com>
2020-04-28 00:22:30 +0530

    Fixed a tiny bug in book list and added list of books for every author

--- graphql-server/schema/schema.js
+++ graphql-server/schema/schema.js
@@ -6,14 +6,18 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
 
 // some dummy data
 var books = [
-  {authorId: "1", name: "Name of the Wind", genre: "Fantasy"},
-  {authorId: "2", name: "The Final Empire", genre: "Fantasy"},
-  {authorId: "3", name: "The Long Earth", genre: "Sci-Fi"}
+  {id: "1", authorId: "1", name: "Name of the Wind", genre: "Fantasy"},
+  {id: "2", authorId: "2", name: "The Final Empire", genre: "Fantasy"},
+  {id: "3", authorId: "2", name: "The Hero of Ages", genre: "Fantasy"},
+  {id: "4", authorId: "3", name: "The Long Earth", genre: "Sci-Fi"},
+  {id: "5", authorId: "3", name: "The Colour of Magic", genre: "Fantasy"},
+  {id: "6", authorId: "3", name: "The Light Fantastic", genre: "Fantasy"}
 ];
 
 var authors = [
@@ -27,7 +31,13 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: {type: GraphQLID},
     name: {type: GraphQLString},
-    age: {type: GraphQLInt}
+    age: {type: GraphQLInt},
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, {authorId: parent.id});
+      }
+    }
   })
 });
 
