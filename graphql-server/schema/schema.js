7c38ec54ecac04105760478f99809204775c17d3
Sanmoh Hombal <devsanmoh@gmail.com>
2020-04-28 00:11:01 +0530

    More dummy data and author tyoe

--- graphql-server/schema/schema.js
+++ graphql-server/schema/schema.js
@@ -1,7 +1,13 @@
 const graphql = require("graphql");
 const _ = require("lodash");
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID} = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = graphql;
 
 // some dummy data
 var books = [
@@ -9,6 +15,21 @@ var books = [
   {id: "2", name: "The Final Empire", genre: "Fantasy"},
   {id: "3", name: "The Long Earth", genre: "Sci-Fi"}
 ];
+
+var authors = [
+  {id: "1", name: "Patrick Rothfuss", age: 44},
+  {id: "2", name: "Brandon Sanderson", age: 42},
+  {id: "3", name: "Terry Pratchett", age: 66}
+];
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: {type: GraphQLID},
+    name: {type: GraphQLString},
+    age: {type: GraphQLInt}
+  })
+});
 
 const BookType = new GraphQLObjectType({
   name: "Book",
